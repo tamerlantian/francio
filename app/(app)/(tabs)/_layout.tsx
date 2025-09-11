2bb74e9a7e517e25240bc58a6355113d5cba47b4
sebastian hoyos <sebastian.h.piedrahita@gmail.com>
2025-09-10 22:03:54 -0500

    feat: implement drawer navigation with custom logout button and header styling

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -3,13 +3,9 @@ import React from 'react';
 
 export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Inicio',
-        }}
-      />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{}} />
+      <Tabs.Screen name="viaje" options={{}} />
     </Tabs>
   );
 }
